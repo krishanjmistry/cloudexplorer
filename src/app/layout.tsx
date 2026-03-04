69ab1d8d8484cd61bf9a4b0072cfcbb079e97710
Krishan Mistry <krishan@krishanmistry.com>
2026-03-04 22:34:55 +0000

    favicon mods

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body className={`${inter.className} antialiased`}>
         <DuckDBProvider>
           <AuthProvider>{children}</AuthProvider>
