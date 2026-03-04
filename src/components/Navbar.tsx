10a21e50e909aa864e703940ba0b1f86a0e9c98d
Krishan Mistry <krishan@krishanmistry.com>
2026-03-04 18:11:26 +0000

    add new cloudicon logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <header className="w-full sticky top-0 z-30">
       <div className="bg-gray-300 shadow-sm px-4 py-3 flex items-center">
-        <div className="flex items-center gap-4">
-          <CloudIcon className="w-12 h-12" aria-hidden="true" />
+        <div className="flex items-center gap-1">
+          <CloudIcon aria-hidden="true" width={40} height={40} />
           <div className="text-2xl font-mono font-bold">cloudexplorer</div>
         </div>
         <div className="flex items-center ml-auto">
